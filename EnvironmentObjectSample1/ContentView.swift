870ff094e14a2a4e72c5e17a237f4d4d987cf12b
morisum <moris@r.sannet.ne.jp>
2024-05-01 17:18:48 +0900

    Initial Commit

---
+++ EnvironmentObjectSample1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnvironmentObjectSample1
+//
+//  Created by MsMacM on 2024/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnvironmentObjectSample1/EnvironmentObjectSample1App.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectSample1App.swift
+//  EnvironmentObjectSample1
+//
+//  Created by MsMacM on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectSample1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
